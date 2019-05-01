bc1edcca2278facdfbb5cc6722b9e9c8f80776c7
ympolka <Valeriya.Yampolska@gmail.com>
2019-05-01 03:36:18 -0400

    Commit

--- AngularJS2/myApp/src/app/app.component.ts
+++ AngularJS2/myApp/src/app/app.component.ts
@@ -1,28 +1,58 @@
-import {Component} from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable, Inject } from '@angular/core';
+import {
+  HttpClient,
+  HttpRequest,
+  HttpHeaders,
+  HttpResponse
+} from '@angular/common/http';
+
+import { Observable } from 'rxjs/Rx';
+import { Component } from '@angular/core';
+import { range } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+import {Doctor} from './doctor.model';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-export class AppComponent{
-
-  constructor(private http: HttpClient){
+export class AppComponent {
+ 
+  doctors : any[];
+  theDoctors : Doctor[];
+  d : Doctor;
+  constructor(private http: HttpClient)
+  {
     this.getData();
+    this.theDoctors=[];
+    this.doctors=[];
   }
 
   url = "https://jsonplaceholder.typicode.com/users";
   title = 'List of our doctors';
-  doctors : any;
-
+  
+  
+    
+     
   public getData(){
     this.http.get(this.url).subscribe((data)=>{
       console.log(data);
-      this.doctors = data;
-    })
+      this.doctors =data; // /???
+      console.log(this.doctors);
+  
+      for (let i = 0, len = this.doctors.length; i < len; i++) {
+       
+        this.d = new Doctor(this.doctors[i].email);
+        this.theDoctors.push(this.d ) ;
+        console.log(this.theDoctors[i]);
+      }
+        
+      });
+   
+
+  
   }
-
-
 }
---
+++ AngularJS2/myApp/src/app/doctor.model.ts
@@ -0,0 +1,23 @@
+export class  Doctor {
+ // ? neskol'ko klassov opisannyx v  одном файле??    tipa skonstruirovat; klass  композитный 
+ 
+email: string;
+id: number;
+name: string;
+phone: string;
+username:  string;
+website: string ;
+votes: number;
+   
+ constructor (email :string  )
+
+     {
+       this.email =email;
+
+
+     }
+
+  }
+  
+
+  
